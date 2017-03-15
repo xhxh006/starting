3e42bb3634d813518744f3db2aa6ef83ecaaea3a
MJ Kim <xhxh006@naver.com>
2017-03-16 00:04:11 +0900

    Hell World..................

---
+++ app/src/main/java/com/ma/mj/testing/HelloApp.java
@@ -0,0 +1,13 @@
+package com.ma.mj.testing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_app);
+    }
+}
